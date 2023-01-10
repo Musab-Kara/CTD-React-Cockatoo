6ebb2ff367a3118b0bb99596905bfdb488e71a84
Musab Kara <msb.kara.94@gmail.com>
2023-01-09 23:20:07 -0500

    created first lesson

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+let todoList = [
+  {
+    id:1,
+    title: "task 1"
+  },
+  {
+    id:2,
+    title: "task 2"
+  },
+  {
+    id:3,
+    title: "task 3"
+  }];
+
+  function App() {
+    return (
+      <>
+      <h2>Todo List</h2>
+      <ul>
+        
+        {todoList.map((item) => {
+          return(
+            <li key={item.id}>
+              <p>{item.title}</p>
+            </li>
+          )
+        })}
+      </ul>
+      </>
+    );
+  }
+
+export default App;
