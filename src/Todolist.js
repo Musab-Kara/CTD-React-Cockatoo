36ab5640c80921ac10ed5d54b72b960d000d56fc
Musab Kara <msb.kara.94@gmail.com>
2023-02-08 21:59:41 -0500

    lesson-1-4  update

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,27 +1,11 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-let todoList = [
-  {
-    id: 1,
-    title: "task 1",
-  },
-  {
-    id: 2,
-    title: "task 2",
-  },
-  {
-    id: 3,
-    title: "task 3",
-  },
-];
-
-function TodoList() {
-
+function TodoList({ todoList }) {
   return (
     <ul>
       {todoList.map((item) => {
-        return <TodoListItem key={item.id} todo={item} />;
+        return <TodoListItem id={item.id} key={item.id} todo={item} />;
       })}
     </ul>
   );
