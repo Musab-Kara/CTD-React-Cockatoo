a5216f22e3f17784d7d3c2bdc91ebcce79cfb9c2
Musab Kara <msb.kara.94@gmail.com>
2023-02-18 09:46:15 -0500

    lesson-1-5

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React ,{useState}  from 'react';
-import TodoList from './Todolist';
-import AddTodoForm from './AddTodoForm';
+import React, { useState, useEffect } from "react";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./Todolist";
 
+const useSemiPersistentState = () => {
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem("value")) || []
+  );
 
+  useEffect(() => {
+    localStorage.setItem("value", JSON.stringify(value));
+  }, [value]);
+  return [value, setValue];
+};
 
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
 
-  function App() {
+  function addTodo(newTodo) {
+    let newTodos = [newTodo, ...todoList];
+    setTodoList(newTodos);
+  }
 
-    const [todoList, setTodoList] = useState([]);
-
-    function addTodo(newTodo) {
-      let newTodos = [newTodo, ...todoList];
-      setTodoList(newTodos);
-      }
-
-    return (
-      <React.Fragment>
+  return (
+    <React.Fragment>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-      </React.Fragment>
-    );
-  } 
+    </React.Fragment>
+  );
+}
 
 export default App;
-
