f15112855a3f07af2a8cd338ae06d975b750dee2
Musab Kara <msb.kara.94@gmail.com>
2023-05-02 23:56:29 -0400

    some css rules editted

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import style from "../style/TodoListItem.module.css";
 import PropTypes from "prop-types";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+import { Switch } from "pretty-checkbox-react";
+import "@djthoms/pretty-checkbox";
 
 function TodoListItem({ todo, onRemoveTodo }) {
   const [checked, setChecked] = useState(false);
@@ -13,26 +15,26 @@ function TodoListItem({ todo, onRemoveTodo }) {
 
   return (
     <div key={todo.id}>
-      
-        <li className={style.ListItem}>
-          <input
-            type="checkbox"
-            checked={checked}
-            onChange={handleChange}
-            className={style.itemCart}
-          />
-          <span className={checked == false ? style.title : style.titleLine}>
-            {todo.fields.Title}
-          </span>
-          <button
-            className={style.button}
-            type="button"
-            onClick={() => onRemoveTodo(todo.id)}
-          >
-            <MdOutlineDeleteOutline size={18} />
-          </button>
-        </li>
-      
+      <li className={style.ListItem}>
+        
+        <Switch
+          shape="fill"
+          color="success"
+          checked={checked}
+          onChange={handleChange}
+        ></Switch>
+
+        <span className={checked == false ? style.title : style.titleLine}>
+          {todo.fields.Title}
+        </span>
+        <button
+          className={style.button}
+          type="button"
+          onClick={() => onRemoveTodo(todo.id)}
+        >
+          <MdOutlineDeleteOutline size={18} />
+        </button>
+      </li>
     </div>
   );
 }
